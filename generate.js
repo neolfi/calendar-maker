0182f731e16a5e128edeb7bc5b1af3676ed29313
Jan Zizka <janzizka@yahoo.com>
2017-10-27 21:24:37 +0200

    Implemented generation of whole year
    Refactored code, added flipping of pages
    and correct arragement for printing.

--- generate.js
+++ generate.js
@@ -2,69 +2,94 @@ const fs = require('fs');
 
 var outfilename = 'output.tex'
 
-file = fs.readFileSync('templates/template-header.tex', {encoding: 'utf8'})
-fs.writeFileSync(outfilename, file)
+var arrange_template = 'templates/template-page-left.tex';
+var arrange_text_pos = 132;
+var arrange_photo_pos = 67;
 
-function output_left() {
-	file = fs.readFileSync('templates/template-page-left.tex', {encoding: 'utf8'})
+var moment = require('moment');
+require('moment/locale/cs')
+
+var namedays = require('./namedays.json')
+var photos = require('./photos.json')
+
+function set_output_left() {
+	arrange_template = 'templates/template-page-left.tex';
+	arrange_text_pos = 132;
+	arrange_photo_pos = 67;
+}
+
+function set_output_right() {
+	arrange_template = 'templates/template-page-right.tex';
+	arrange_text_pos = 25;
+	arrange_photo_pos = 143;
+}
+
+function output_template() {
+	file = fs.readFileSync(arrange_template, {encoding: 'utf8'})
 	fs.appendFileSync(outfilename, file)
 }
 
 function output_day( day, dayofweek ) {
 	var offset = 27 + 17 * dayofweek
-	var date = '\\PlaceText{132mm}{' + offset + 'mm}{\\huge{' + day + '}}\n'
+	var date = '\\PlaceText{' + arrange_text_pos + 'mm}{' + offset + 'mm}{\\huge{' + day + '}}\n'
 	fs.appendFileSync(outfilename, date)
 }
 
 function output_name( name, dayofweek ) {
 	var offset = 31 + 17 * dayofweek
-	var name = '\\PlaceText{132mm}{' + offset + 'mm}{\\emph{\\tiny{' + name + '}}}\n'
+	var name = '\\PlaceText{' + arrange_text_pos + 'mm}{' + offset + 'mm}{\\emph{\\tiny{' + name + '}}}\n'
 	fs.appendFileSync(outfilename, name) 
 }
 
 function output_photo( photo ) {
 	fs.appendFileSync(outfilename, '\\begin{tikzpicture}[remember picture,overlay]')
 	fs.appendFileSync(outfilename, '  \\node[outer sep=0pt,inner sep=0pt,anchor=north]')
-	fs.appendFileSync(outfilename, '    at ([xshift=67mm,yshift=-26mm]current page.north west)')
+	fs.appendFileSync(outfilename, '    at ([xshift=' + arrange_photo_pos + 'mm,yshift=-26mm]current page.north west)')
 	fs.appendFileSync(outfilename, '    {\\includegraphics[max width=104mm, max height=104mm]{' + photo + '}};')
 	fs.appendFileSync(outfilename, '\\end{tikzpicture}')
 }
+
 
 function output_new_page() {
 	fs.appendFileSync(outfilename, '\\newpage')
 }
 
-var moment = require('moment');
-require('moment/locale/cs')
-
-var date = moment("2017-01-01");
-
-var startweekday = date.weekday()
-
-date.add(-startweekday, 'days')
-
-var namedays = require('./namedays.json')
-var photos = require('./photos.json')
-
-for(var day = 0 ; day < 14+startweekday; day++ )
-{
-	var dayofweek = date.weekday()
-	if ( day % 7 == 0 ) {
-		output_left()
-		photo = photos[date.week().toString()]
-		if ( photo != undefined ) output_photo(photo)
-		console.log("=== Week " + date.week().toString())
-	}
-	output_day(date.format('D'), day % 7)
-	nameday = namedays[date.format('D.M.')]
-	if ( nameday != undefined) output_name(nameday, day % 7)
-	if ( dayofweek == 6 ) 
-	{
-		output_new_page()
-	}
-	date.add(1, 'days')
+function get_week( week ) {
+	var date = moment("2017-01-01");
+	var startweekday = date.weekday()
+	date.add(-startweekday + week*7, 'days')
+	return date
 }
 
+function output_week( week ) {
+	var date = get_week( week );
+
+	output_template()
+	photo = photos[date.week().toString()]
+	if ( photo != undefined ) output_photo(photo)
+	console.log("=== Week " + date.week().toString())
+	for(var day = 0 ; day < 7; day++ )
+	{
+		output_day(date.format('D'), day % 7)
+		nameday = namedays[date.format('D.M.')]
+		if ( nameday != undefined) output_name(nameday, day % 7)
+		date.add(1, 'days')
+	}
+	output_new_page()
+}
+
+
+file = fs.readFileSync('templates/template-header.tex', {encoding: 'utf8'})
+fs.writeFileSync(outfilename, file)
+
+for ( var week = 0; week < 26; week+=2 ) {
+	set_output_left();
+	output_week( week );
+	output_week( week+1 );
+	set_output_right();
+	output_week( 51-week );
+	output_week( 51-week-1 );
+}
 
 file = fs.readFileSync('templates/template-footer.tex', {encoding: 'utf8'})
 fs.appendFileSync(outfilename, file)
